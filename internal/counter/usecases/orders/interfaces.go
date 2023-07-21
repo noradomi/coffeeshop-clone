d1de27b82544934f5da89d89953412581f656783
phucvt <fakermsi1999@gmail.com>
2023-07-21 17:04:58 +0700

    [counter-service] Implement orders_postgres.go in infras/repo

--- internal/counter/usecases/orders/interfaces.go
+++ internal/counter/usecases/orders/interfaces.go
@@ -1,3 +1,22 @@
 package orders
 
-//type
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/noradomi/coffeeshop-clone/internal/counter/domain"
+)
+
+type (
+	OrderRepo interface {
+		GetAll(context.Context) ([]*domain.Order, error)
+		GetByID(context.Context, uuid.UUID) (*domain.Order, error)
+		Create(context.Context, *domain.Order) error
+		Update(context.Context, *domain.Order) (*domain.Order, error)
+	}
+
+	UseCase interface {
+		GetListOrderFulfillment(context.Context) ([]*domain.Order, error)
+		PlaceOrder(context.Context, *domain.PlaceOrderModel) error
+	}
+)
